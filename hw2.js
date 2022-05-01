ef46253111caf0711954fc7280129b39ef436d32
Chainarong Mundee <chainarong9846@gmail.com>
2022-05-02 00:43:35 +0700

    update 2

--- hw2.js
+++ hw2.js
@@ -1,9 +1,18 @@
 let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let temp1 = []
-x=0
-y=1
-for(let i=0; i<characters.length; i++) {
-    temp1.push(characters[i])
-}
-temp1[x] = temp1.splice(y, 1, temp1[x])[0]
-console.log(temp1.join(""))
+  let temp1 = []  
+  let temp2 = []
+  let mergeArray = []
+  let Ans = []
+  for(let i=0; i<characters.length; i++) {
+      if(i%2!=0) {
+          temp1.push(characters[i])
+      }
+      if(i%2==0) {
+          temp2.push(characters[i])
+      }
+  }
+  let newArray = temp1.map((e, i) => e + temp2[i]);
+  for(let i=0; i<newArray.length; i++) {
+    Ans.push(newArray[i].split(","))
+  }
+console.log(Ans.join(""))
